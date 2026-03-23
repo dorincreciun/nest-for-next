e99dffc8ae60180404ed7b3007cbb5dfd0dd093e
dorincreciun <creciundorin25@gmail.com>
2026-03-23 16:51:09 +0200

    chore: init project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import {Module} from '@nestjs/common';
 import {PrismaModule} from './module/prisma/prisma.module';
+import { AuthModule } from './module/auth/auth.module';
+import { UserModule } from './module/user/user.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule, UserModule],
 })
 export class AppModule {}
